5d2d92beb2f4a953d0ddef1c8fa1e52085a239f9
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-03-06 09:58:58 +0000

    HR3. Update.

--- main.py
+++ main.py
@@ -76,7 +76,6 @@ def shared_resources_planning(working_directory, specification_filename):
     processed_results = transmission_network.process_results(tn_model, results)
     transmission_network.write_optimization_results_to_excel(processed_results)
 
-    '''
     distribution_networks = planning_problem.distribution_networks
     candidate_solution = planning_problem.get_initial_candidate_solution()
     for node_id in distribution_networks:
@@ -86,7 +85,6 @@ def shared_resources_planning(working_directory, specification_filename):
         results = distribution_network.optimize(dn_model)
         processed_results = distribution_network.process_results(dn_model, results)
         distribution_network.write_optimization_results_to_excel(processed_results)
-    '''
 
     print('==========================================================================================================')
     print('                                                 END                                                      ')
